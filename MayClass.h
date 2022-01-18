b12f08db78e5613884acaf7948a72fb2c0850187
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2022-01-18 09:29:19 +0000

    Initial commit

---
+++ MayClass.h
@@ -0,0 +1,18 @@
+#ifndef __MYCLASS_H__
+#define __MYCLASS_H__
+
+class MyClass {
+    private:
+    int a;
+
+    public:
+    MyClass() {
+
+    };
+
+    int doSomething(int a) {
+        return a+1;
+    }
+};
+
+#endif
---
+++ Stuff.h
@@ -0,0 +1,10 @@
+#ifndef __STUFF_H__
+#define __STUFF_H__
+
+typedef struct {
+    int a;
+    float b;
+} Dat_t;
+
+#endif
+
